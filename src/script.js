375fdc4e0a7ef7df15838899b4c8ab326ae850e5
Janett Gerrard <mikanchik@gmail.com>
2024-04-06 12:02:10 +0100

    fetch request working

--- src/script.js
+++ src/script.js
@@ -1,31 +1,41 @@
-const app = () => { 
-// Query Selectors
-const form = document.querySelector('.chat-from')
-const formInput = document.getElementById('chat-input')
+import config from '../config.js'
 
-// Functions 
-// const apiFetch = async() => {
-//     try {
-//         const response = fetch(, {
-//             method: "POST",
-//             headers: {
-//               "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 message: [],
-//                 model: 'gbt-3.5-turbo',
-//             })
-//         })
-//     } catch (error) {
-//         console.log(error , 'Failed to fetch');
-//     }
-// }
+const app = () => {
+    // Query Selectors
+    const form = document.querySelector('.chat-form')
 
-// Event Listeners
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
-    console.log(event.target);
-    // apiFetch()
-})
+    // Functions
+    const apiFetch = async () => {
+        const formInput = document.getElementById('chat-input').value
+        try {
+            const response = await fetch(config.apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${config.apiKey}`
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [{ role: 'user', content: formInput }]
+                })
+            })
+            const data = await response.json()
+            if(response.ok) {
+              console.log(data)
+              return data
+            } else {
+              throw new Error
+            }
+        } catch (error) {
+            console.log(error, 'Failed to fetch');
+        }
+    }
+
+    // Event Listeners
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        const apiResponse = apiFetch()
+    })
 
 }
+app()
