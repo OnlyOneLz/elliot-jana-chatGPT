e8bbc3d104567fc9e8a83cabb982369797894a5c
Janett Gerrard <mikanchik@gmail.com>
2024-04-06 13:45:28 +0100

    send whole context of conversation in every message

--- src/script.js
+++ src/script.js
@@ -2,52 +2,64 @@ import config from '../config.js'
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 const app = () => {
-    // Query Selectors
-    const form = document.querySelector('.chat-form')
-    const messagesDiv = document.querySelector("#chat-messages")
+  // Query Selectors
+  const form = document.querySelector('.chat-form')
+  const formInput = document.getElementById('chat-input')
+  const messagesDiv = document.querySelector("#chat-messages")
 
-    const displayChatMessage =  (content, role) => {
-      console.log(content)
-      const thisMessage = document.createElement("div")
-      thisMessage.classList.add(role)
-      thisMessage.innerHTML = content
-      messagesDiv.appendChild(thisMessage)
-    }
+  // state
+  const messageHistory = []
 
-    // Functions
-    const apiFetch = async () => {
-        const formInput = document.getElementById('chat-input').value
-        displayChatMessage(formInput, "question")
-        try {
-            const response = await fetch(config.apiUrl, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${config.apiKey}`
-                },
-                body: JSON.stringify({
-                    model: 'gpt-3.5-turbo',
-                    messages: [{ role: 'user', content: formInput }]
-                })
-            })
-            const data = await response.json()
-            if(response.ok) {
-              console.log(data)
-              return data
-            } else {
-              throw new Error
-            }
-        } catch (error) {
-            console.log(error, 'Failed to fetch');
-        }
-    }
-
-    // Event Listeners
-    form.addEventListener('submit',async function (event) {
-        event.preventDefault()
-        const apiResponse = await apiFetch()
-        const messageContent = apiResponse.choices[0].message.content
-        displayChatMessage(marked.parse(messageContent), "answer")
+  // Functions
+  const updateMessageHistory = (data, role) => {
+    messageHistory.push({
+      role: role,
+      content: data
     })
+  }
+
+  const displayChatMessage = (content, role) => {
+    const thisMessage = document.createElement("div")
+    thisMessage.classList.add(role)
+    thisMessage.innerHTML = content
+    messagesDiv.appendChild(thisMessage)
+  }
+
+  const apiFetch = async () => {
+    try {
+      const response = await fetch(config.apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${config.apiKey}`
+        },
+        body: JSON.stringify({
+          model: 'gpt-3.5-turbo',
+          messages: messageHistory
+        })
+      })
+      const data = await response.json()
+      if (response.ok) {
+        console.log(data)
+        return data
+      } else {
+        throw new Error
+      }
+    } catch (error) {
+      console.log(error, 'Failed to fetch');
+    }
+  }
+
+  // Event Listeners
+  form.addEventListener('submit', async function (event) {
+    event.preventDefault()
+    const query = formInput.value
+    displayChatMessage(query, "question")
+    updateMessageHistory(query, "user")
+    const apiResponse = await apiFetch()
+    const messageContent = apiResponse.choices[0].message.content
+    updateMessageHistory(messageContent, "assistant")
+    displayChatMessage(marked.parse(messageContent), "answer")
+  })
 }
 app()
