dc4069fc673adadde7928f12f52d3f0a57dcb56d
Janett Gerrard <mikanchik@gmail.com>
2024-05-02 09:43:58 +0100

    test file for api fetch function

---
+++ tests/apiFetchTest.js
@@ -0,0 +1,12 @@
+import { test } from "./test-helpers.js";
+import { equal } from "./test-helpers.js";
+import apiFetch from "../src/apiFetch.js";
+
+// we want to check that we get status 200
+test("it returns status 200", async () => {
+  const res = await apiFetch([{
+    role: "user",
+    content: "hello"
+  }])
+  equal(res.response.status, 200)
+})
