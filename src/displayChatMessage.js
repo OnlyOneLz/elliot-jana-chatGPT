0e0fcb4e42926994f029f1aad5fdf99ae5a52616
Janett Gerrard <mikanchik@gmail.com>
2024-05-07 21:15:10 +0100

    refactor chat message display

--- src/displayChatMessage.js
+++ src/displayChatMessage.js
@@ -1,22 +1,24 @@
 import displayTextLetterByLetter from "./displayTextLetterByLetter.js";
 
 const displayChatMessage = (content, role, messagesDiv) => {
-    const thisMessage = document.createElement("div");
-    thisMessage.classList.add(role);
-    const messageTitle = document.createElement("h2")
-    messageTitle.classList.add(role);
+  const thisMessage = document.createElement("div");
+  thisMessage.classList.add(role, "message");
 
+  const messageTitle = document.createElement("h2")
+  messageTitle.classList.add(role);
+  const messageText = document.createElement("p")
 
-    if (role === 'question'){
-      messageTitle.innerHTML = 'You'
-    } else {
-      messageTitle.innerHTML = 'Chat GPT'
-    }
+  if (role === 'question') {
+    messageTitle.innerHTML = 'You'
+  } else {
+    messageTitle.innerHTML = 'Chat GPT'
+  }
 
-      displayTextLetterByLetter(content, thisMessage, role);
+  displayTextLetterByLetter(content, messageText, role);
+  thisMessage.appendChild(messageTitle);
+  thisMessage.appendChild(messageText);
 
-      messagesDiv.appendChild(messageTitle);
-      messagesDiv.appendChild(thisMessage)
+  messagesDiv.appendChild(thisMessage);
 }
 
 export default displayChatMessage
