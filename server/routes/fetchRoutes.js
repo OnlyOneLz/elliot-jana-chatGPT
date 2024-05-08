1b6469e70b09bf980a6635781458c09f6f388af5
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-05-08 20:59:20 +0100

    server setup

--- server/routes/fetchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const apiFetch = require("../controller/fetch");
-
-router.post("/Api", apiFetch);
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,42 @@
-const express = require("express");
 const cors = require("cors");
-const apiRoutes = require("./routes/fetchRoutes");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const router = express.Router();
 require("dotenv").config();
 
 const app = express();
 
-app.use(express.json());
+app.use("/app/", router);
 app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.use("/user", apiRoutes);
+app.post("/Api-fetch", async (req, res) => {
+  const messageHistory = req.body.messageHistory;
+  console.log(messageHistory);
+  try {
+    const response = await fetch(process.env.API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: messageHistory,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      res.json(data);
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    console.log(error, "Failed to fetch");
+  }
+});
 
 app.listen(4005, () => console.log("Server listening on port 4005"));
